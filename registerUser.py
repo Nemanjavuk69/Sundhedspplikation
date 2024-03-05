5cd4bfaf8da13c931868daeb17320b5276e716af
nemanjavuk69 <Bela_Legenda69>
2024-03-06 00:39:10 +0100

    Added register user and design for the webpage

--- registerUser.py
+++ registerUser.py
@@ -2,15 +2,12 @@ from flask import Blueprint, request, render_template, redirect, url_for, flash
 import csv
 from hashing import hash_string
 
-# Create a Flask Blueprint named 'register_user'
 register_user = Blueprint('register_user', __name__,
                           template_folder='templates')
 
-# Check if the username already exists in the CSV file
-
 
 def username_exists(username):
-    with open('users.csv', 'r', newline='') as csvfile:
+    with open('users.csv', mode='r', newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             if row[0] == username:
@@ -25,16 +22,14 @@ def register():
         password = request.form['password']
 
         if username_exists(username):
-            # If the username already exists, inform the user
-            flash('Username already in use')
+            flash('The username is already in use', 'error')  # Flash message
             return redirect(url_for('register_user.register'))
-        else:
-            # Hash the password and save the new user to the CSV file
-            hashed_password = hash_string(password)
-            with open('users.csv', 'a', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow([username, hashed_password])
-            flash('Registration successful! Please login.')
-            return redirect(url_for('register_user.register'))
+
+        hashed_password = hash_string(password)
+        with open('users.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([username, hashed_password])
+        flash('Registration successful! Please login.', 'success')
+        return redirect(url_for('register_user.register'))
 
     return render_template('registerUser.html')
